78389d367dd50e553a0fce05685773241fa4ef87
Arvind <arvindcool2022@gmail.com>
2023-12-21 05:43:08 +0530

    Chapter 5 completed

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,6 +1,9 @@
 import { Inter } from 'next/font/google';
 import { Lusitana } from 'next/font/google';
 
+// console.log(Inter, Lusitana);
+//!Font loaders must be called and assigned to a const in the module scope
+
 export const inter = Inter({ subsets: ['latin'] });
 
 export const lusitana = Lusitana({
