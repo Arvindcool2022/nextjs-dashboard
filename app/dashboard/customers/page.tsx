fbedbad208b73cd00eb56fd74f550215d904bd3a
Arvind <arvindcool2022@gmail.com>
2023-12-21 18:17:45 +0530

    ch-09 completed

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,33 @@
-export default function Page() {
-  return <p>Customers Page</p>;
+import Image from 'next/image';
+export default async function Page() {
+  const response = await fetch('https://dummyjson.com/products/1');
+  const data = await response.json();
+  console.log(data);
+  return (
+    <div>
+      <Image src={data.images[0]} alt={data.title} width={500} height={400} />
+      <h1 className="mt-4 text-xl font-bold">{data.title}</h1>
+      <p>{data.description}</p>
+    </div>
+  );
 }
+
+// {
+//   id: 1,
+//   title: 'iPhone 9',
+//   description: 'An apple mobile which is nothing like apple',
+//   price: 549,
+//   discountPercentage: 12.96,
+//   rating: 4.69,
+//   stock: 94,
+//   brand: 'Apple',
+//   category: 'smartphones',
+//   thumbnail: 'https://i.dummyjson.com/data/products/1/thumbnail.jpg',
+//   images: [
+//     'https://i.dummyjson.com/data/products/1/1.jpg',
+//     'https://i.dummyjson.com/data/products/1/2.jpg',
+//     'https://i.dummyjson.com/data/products/1/3.jpg',
+//     'https://i.dummyjson.com/data/products/1/4.jpg',
+//     'https://i.dummyjson.com/data/products/1/thumbnail.jpg'
+//   ]
+// }
--- next.config.js
+++ next.config.js
@@ -1,4 +1,27 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
 
-module.exports = nextConfig;
+// module.exports = nextConfig;
+
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.dummyjson.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+// images - The images config object
+// remotePatterns - Whitelisted hostnames for next/image
+// protocol - The protocol used (https)
+// hostname - The hostname used in your image src
+// port - Port if applicable
+// pathname - Pathname pattern to match images on this host
+// Using '/**' will match all paths on 'i.dummyjson.com'.
+
+// This will configure Next.js to allow your next/image components to load images from 'i.dummyjson.com' without errors.
